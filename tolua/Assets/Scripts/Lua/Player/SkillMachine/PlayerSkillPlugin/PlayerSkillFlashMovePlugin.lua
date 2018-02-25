d016bb1360e44063b96af56e713e7991a28a6bac
Enanyy <742216502@qq.com>
2018-02-25 22:39:12 +0800

    Flash

--- tolua/Assets/Scripts/Lua/Player/SkillMachine/PlayerSkillPlugin/PlayerSkillFlashMovePlugin.lua
+++ tolua/Assets/Scripts/Lua/Player/SkillMachine/PlayerSkillPlugin/PlayerSkillFlashMovePlugin.lua
@@ -40,6 +40,8 @@ function PlayerSkillFlashMovePlugin:OnExecute()
 
         self.mTargetPosition = self.mOriginalPosition + self.machine.mPlayerCharacter.transform.forward* self.mDistance
 
+        --self.mTargetPosition = self:CheckFlashPosition(self.mTargetPosition)
+
         if self.mPlayerSkillState.mRunTime <= self.mDuration then
             local factor = self.mPlayerSkillState.mRunTime * 1.0 / self.mDuration
             self.machine.mPlayerCharacter.transform.position = self.mOriginalPosition * (1 - factor) + self.mTargetPosition * factor
@@ -57,5 +59,35 @@ function PlayerSkillFlashMovePlugin:OnExit()
     if self.mNavMeshAgent then
         self.mNavMeshAgent.enabled = true
     end
-    
+
+end
+
+function PlayerSkillFlashMovePlugin:CheckFlashPosition(targetPosition)
+
+    local tmpFlag, tmpHit = NavMesh.SamplePosition(targetPosition, nil, 0.1, NavMesh.AllAreas)
+    if tmpFlag then
+
+        print(1)
+       return targetPosition
+
+    else
+
+        local tmpPosition = targetPosition + self.machine.mPlayerCharacter.transform.forward* self.mDistance * 0.5
+
+        tmpFlag, tmpHit = NavMesh.SamplePosition(targetPosition, nil, 0.1, NavMesh.AllAreas)
+
+        if tmpFlag then
+
+            print(2)
+            
+            return tmpPosition
+
+        else
+
+            print(3)
+            
+            return self.machine.mPlayerCharacter.transform.position
+        end
+
+    end
 end
