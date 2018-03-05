5031d81e915345a5aa24d0985008f71b2656ab45
Enanyy <742216502@qq.com>
2018-03-05 09:48:33 +0800

    Helper

---
+++ tolua/Assets/Scripts/CSharp/Helper.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections;
+
+public static class Helper  {
+
+    static public int MakeMask(params int[] layers)
+    {
+        int mask = 0;
+        for (int i = 0; i < layers.Length; i++)
+        {
+            mask |= 1 << layers[i];
+        }
+        return mask;
+    }
+}
